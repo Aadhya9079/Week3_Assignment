00e8c5abeb295af384baf2cc2d544449a2ce6fae
Aadhya9079 <146864352+Aadhya9079@users.noreply.github.com>
2024-09-18 09:24:09 +0530

    Add files via upload

---
+++ Constant3.java
@@ -0,0 +1,16 @@
+/***
+ * Here is the constant file of Cricket match Scorer App.
+ * Owner-Name: Aadhya Goyal
+ * Date Of Creation:17-09-24
+ */
+
+public class Constant {
+     static String StartMessage = "\n                         Welcome to the Cricket Match Scorer App....";
+     static String CreateTeamMessage = "\n\n Create your team...\n Enter the number of players in your team(11-30):";
+     static String YesOrNoMessage = "Do you want to create a new team?(yes/no): ";
+     static String InvalidMessage ="Invalid number of players.Please enter a number between 11 to 30.";
+     static String RunInput = "Enter (runs,no ball,wide ball) for each ball:";
+     static String InvalidRunCount ="Invalid run count.";
+     static String[] Player1 = {"Aadhya","Aanchal","Avadhi","Adity","Gungun","Vijju","Priyanshu","Lucky","Priyanka","Aayushi","Harsh"};
+     static String[] Player2={"Ajay","Abhilash","Rupam","Shubham","Anuj","Milan","Priyanshu","Tanuj","Khushi","Khusbhu","Harsh"};
+}
